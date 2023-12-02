8e59e68665f087f2511f2808e5899cdb62d403ce
Gokamoto25 <okamoto.direc@gmail.com>
2023-12-02 03:22:40 -0800

    4-puts.c fixed

--- hello_world/4-puts.c
+++ hello_world/4-puts.c
@@ -1,9 +1,11 @@
 	#include <stdio.h>
 
-	int main(void)
 	/**
-	 * main - prints a string with the puts function
+	 * main - prints "Programming is like building a multilingual puzzle, followed by a new line.
+	 * Return: 0
 	 */
+
+	int main(void)
 	{
 		puts("\"Programming is like building a multilingual puzzle");
 		return (0);
