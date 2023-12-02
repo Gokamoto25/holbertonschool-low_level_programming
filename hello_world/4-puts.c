92efd3d748112a563e62d82596a753b648001db2
Gokamoto25 <okamoto.direc@gmail.com>
2023-12-02 01:35:09 -0800

    4-puts.c fixed

--- hello_world/4-puts.c
+++ hello_world/4-puts.c
@@ -1,5 +1,11 @@
 	#include <stdio.h>
 
+	/**
+	 * main - imprime una cadena de caracteres establecida
+	 *
+	 * Descripcion - la funcion puts imprime el string "Programming is like building a multilingual puzzle con un salto de linea
+	 * Return: 0
+	 */
 	int main(void)
 	{
 		puts("\"Programming is like building a multilingual puzzle");
