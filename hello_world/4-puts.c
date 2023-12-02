f1bfbab1c8d33c6e846ba095f53d0edd35139ee6
Gokamoto25 <okamoto.direc@gmail.com>
2023-12-02 03:32:47 -0800

    4-puts.c

--- hello_world/4-puts.c
+++ hello_world/4-puts.c
@@ -1,8 +1,7 @@
 	#include <stdio.h>
 
 	/**
-	 * main - prints "Programming is like building a multilingual puzzle
-	 * followed by a new line
+	 * main - prints a string with the puts function
 	 * Return: 0
 	 */
 
