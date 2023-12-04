801e6a4d62be4d222c4d8e19f1b6c5f21b8d8319
Gokamoto25 <okamoto.direc@gmail.com>
2023-12-04 14:31:07 -0800

    6-abs created

---
+++ functions_nested_loops/6-abs.c
@@ -0,0 +1,17 @@
+#include "main.h"
+/**
+ * _abs - computes the absolute value of an integer
+ * @i: parametro a ser verificado
+ * Return: 0
+ */
+int _abs(int i)
+{
+if (i < 0)
+{
+	int valorAbsoluto;
+	
+	valorAbsoluto = i * -1;
+	return (valorAbsoluto);
+}
+return (i);
+}
