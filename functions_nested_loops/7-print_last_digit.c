05f113cebd8280a1e6f8c5b5b0bae78021026816
Gokamoto25 <okamoto.direc@gmail.com>
2023-12-04 14:59:33 -0800

    7-last_digit created

--- functions_nested_loops/7-print_last_digit.c
+++ functions_nested_loops/7-print_last_digit.c
@@ -2,7 +2,7 @@
 /**
  * print_last_digit - prints the last digit of a number
  * @i: parametro para ser verificado
- * Return: 
+ * Return: ultimoDigito
  */
 int print_last_digit(int i)
 {
@@ -10,7 +10,7 @@ int ultimoDigito;
 ultimoDigito = i % 10;
 if (ultimoDigito < 0)
 {
-	ultimoDigito = ultimoDigito * -1;
+ultimoDigito = ultimoDigito * -1;
 }
 _putchar(ultimoDigito + '0');
 return (ultimoDigito);
