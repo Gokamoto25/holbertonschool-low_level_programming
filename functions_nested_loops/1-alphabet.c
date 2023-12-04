ea99dfdc89dda422ae75bbc6c20b1c64a2fc4bab
Gokamoto25 <okamoto.direc@gmail.com>
2023-12-04 11:07:35 -0800

    1-alphabet created

--- functions_nested_loops/1-alphabet.c
+++ functions_nested_loops/1-alphabet.c
@@ -1,6 +1,6 @@
 #include "main.h"
 /**
- * main - prints the alphabet,
+ * print_alphabet - prints the alphabet,
  * in lowercase, followed by a new line
  * Return: void
  */
@@ -9,7 +9,7 @@ void print_alphabet(void)
 	char ch;
 	for (ch = 'a' ; ch <= 'z' ; ch++)
 	{
-		_putchar(ch);
+	_putchar(ch);
 	}
 	_putchar('\n');
 }
