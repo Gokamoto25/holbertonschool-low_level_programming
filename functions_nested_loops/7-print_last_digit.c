9b1346c84c5bbfe1dd227792a5c08830c11b0834
Gokamoto25 <okamoto.direc@gmail.com>
2023-12-04 14:57:27 -0800

    7-last_digit created

---
+++ functions_nested_loops/7-print_last_digit.c
@@ -0,0 +1,17 @@
+#include "main.h"
+/**
+ * print_last_digit - prints the last digit of a number
+ * @i: parametro para ser verificado
+ * Return: 
+ */
+int print_last_digit(int i)
+{
+int ultimoDigito;
+ultimoDigito = i % 10;
+if (ultimoDigito < 0)
+{
+	ultimoDigito = ultimoDigito * -1;
+}
+_putchar(ultimoDigito + '0');
+return (ultimoDigito);
+}
