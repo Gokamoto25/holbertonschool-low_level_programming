6cbc49a87f9038d7aa38bc113ec41556b6ae0843
Gokamoto25 <okamoto.direc@gmail.com>
2023-12-04 03:57:53 -0800

    9-print_comb.c created

--- variables_if_else_while/9-print_comb.c
+++ variables_if_else_while/9-print_comb.c
@@ -7,15 +7,20 @@
 int main(void)
 {
 	int i;
+
 	for (i = '0' ; i <= '9' ; i++)
 	{
 		putchar(i);
+
 	if (i != '9')
 	{
 		putchar(',');
+
 		putchar(' ');
 	}
 	}
+
 	putchar('\n');
+
 	return (0);
 }
