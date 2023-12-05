3e67f38d27e48b846fa15f57b595becc4a8338ec
Gokamoto25 <okamoto.direc@gmail.com>
2023-12-05 06:09:08 -0800

    10-add created

--- functions_nested_loops/10-add.c
+++ functions_nested_loops/10-add.c
@@ -2,6 +2,8 @@
 /**
  * add - adds two integers
  * and returns the result
+ * @nmb1: primer numero a sumar
+ * @nmb2: segundo numero para sumar
  * Return: two numbers
  */
 int add(int nmb1, int nmb2)
