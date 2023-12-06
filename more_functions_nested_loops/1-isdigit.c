936b55f11dd2faa756b8c9212213e3995b65d344
Gokamoto25 <okamoto.direc@gmail.com>
2023-12-06 08:19:07 -0800

    1-isdigit.c created

---
+++ more_functions_nested_loops/1-isdigit.c
@@ -0,0 +1,14 @@
+#include "main.h"
+/**
+ * _isdigit - checks for a digit (0 through 9)
+ * @c: parametro a ser verificado
+ * Return: 1 if c is a digit, 0 otherwise
+ */
+int _isdigit(int c)
+{
+	if (c >= 49 && c <= 57)
+	{
+		return (1);
+	}
+	return (o);
+}
