1a6c5747c788e99ec7ee2a7eb973f7fe70809519
Gokamoto25 <okamoto.direc@gmail.com>
2023-12-06 08:34:57 -0800

    2-mul created

---
+++ more_functions_nested_loops/2-mul.c
@@ -0,0 +1,11 @@
+#include "main.h"
+/**
+ * mul - multiplies two integers
+ * @a: primer entero a multiplicar
+ * @b: segundo entero a multiplicar
+ * Return: 0
+ */
+int mul(int a, int b)
+{
+	return (a * b);
+}
