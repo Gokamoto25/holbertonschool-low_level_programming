15836097e41ac49476e20fb68e24bdd8f41751ae
Gokamoto25 <okamoto.direc@gmail.com>
2023-12-06 17:41:15 -0800

    6-print_line.c created

---
+++ more_functions_nested_loops/6-print_line.c
@@ -0,0 +1,21 @@
+#include "main.h"
+/**
+ * print_line - draws a straight line in the terminal
+ * Return: void
+ */
+void print_line(int n)
+{
+	int a;
+	if (n <= 0)
+	{
+	_putchar('\n');
+	}
+	else
+	{
+	for (a = 0; a < n; a++)
+	{
+	_putchar(95);
+	}
+	_putchar('\n');
+	}
+}
