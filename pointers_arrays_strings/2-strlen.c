cfa73020aaff5e8bbab630b5663e24b2b49010d5
Gokamoto25 <okamoto.direc@gmail.com>
2023-12-08 15:13:30 -0800

    2-strlen created

---
+++ pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,18 @@
+#include "main.h"
+/**
+ * _strlen - returns the length of a string
+ * @str: string a medir length
+ * Return: length
+ */
+int _strlen(char *str)
+{
+
+int length = 0;
+
+while(*str++)
+
+length++;
+
+return (length);
+
+}
