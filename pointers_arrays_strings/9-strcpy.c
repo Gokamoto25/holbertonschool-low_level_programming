11e1f285838f062d8359613ddd62a0a6b63bf1a3
Gokamoto25 <okamoto.direc@gmail.com>
2023-12-09 14:54:24 -0800

    9-strcpy created

--- pointers_arrays_strings/9-strcpy.c
+++ pointers_arrays_strings/9-strcpy.c
@@ -2,9 +2,9 @@
 /**
  * *_strcpy - copies the string pointed to by src,
  * including the terminating null byte (\0)
- * @dest: string con nuevo valor
- * @src: string para copiar el valor
- * Return: dest
+ * @dest:
+ * @src:
+ * Return:
  */
 char *_strcpy(char *dest, char *src)
 {
@@ -17,5 +17,4 @@ char *_strcpy(char *dest, char *src)
 	dest[i++] = '\0';
 
 	return (dest);
-}
-		
+}	
