648cc758bdf6a0fda67a3183bc166721fcebf7d1
Gokamoto25 <okamoto.direc@gmail.com>
2023-12-09 16:47:09 -0800

    100-atoi created

---
+++ pointers_arrays_strings/100-atoi.c
@@ -0,0 +1,40 @@
+#include "main.h"
+/**
+ * _atoi - convert a string to an integer
+ * @s:
+ * Return: 0 if there are no numbers
+ */
+int _atoi(char *s)
+{
+	int i = 0;
+	
+	unsigned int ni = 0;
+	
+	int min = 1;
+
+	int isi = 0;
+
+	while (s[i])
+	{
+	if (s[i] == 45)
+	{
+	min *= -1;
+	}
+	while (s[i] >= 48 && s[i] <= 57)
+	{
+	isi = 1;
+	
+	ni = (ni * 10) + (s[i] - '0');
+	
+	i++;
+	}
+	if (isi == 1)
+	{
+	break;
+	}
+	i++;
+	}
+	ni *= min;
+
+	return (ni);
+}
