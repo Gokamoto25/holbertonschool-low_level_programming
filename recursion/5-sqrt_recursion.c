606f660a2f2c838da981d9d5e4cbc4a6aeddecb4
Gokamoto25 <okamoto.direc@gmail.com>
2023-12-11 22:38:32 -0800

    5-sqrt created

---
+++ recursion/5-sqrt_recursion.c
@@ -0,0 +1,31 @@
+#include "main.h"
+int _raiz(int n, int i);
+/**
+ * _raiz - calcula la raiz del numero natural
+ * @n: numero a calcular la raiz
+ * @i: numero a iterar
+ * Return: la raiz del numero natural
+ */
+
+int _sqrt_recursion(int n)
+{
+	return (_raiz(n, 1));
+}
+/**
+ * _sqrt_recursion - returns the natural square root of a number
+ *@n: numero a calcular la raiz
+ *@i: numero raíz
+ *Return: the natural square root of a number
+ */
+int _raiz(int n, int i)
+{
+	int raiz = i * i;
+
+	if (raiz > n)
+		return (-1);
+
+	if (raiz == n)
+		return (i);
+
+	return (_raiz(n, i + 1));
+}
