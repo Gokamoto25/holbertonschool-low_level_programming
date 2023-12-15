6e058d64a75cacd40a4bbbf74e5525821840f933
Gokamoto25 <okamoto.direc@gmail.com>
2023-12-14 23:41:53 -0800

    a created

--- malloc_free/0-create_array.c
+++ malloc_free/0-create_array.c
@@ -4,7 +4,7 @@
  * create_array - creates an array of chars,
  * and initializes it with a specific char
  * @size: the size of the array
- * @c. the char to fill in the array
+ * @c: the char to fill in the array
  * Return: the array
  */
 char *create_array(unsigned int size, char c)
